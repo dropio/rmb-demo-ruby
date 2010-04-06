a72cbafc2ec9f4f1175c329adb7e2bc4a8233978
Eric Skiff <eric@dropio.com>
2010-04-06 16:44:11 -0400

    idiot fix

--- app/controllers/get_drop_controller.rb
+++ app/controllers/get_drop_controller.rb
@@ -5,10 +5,11 @@ class GetDropController < ApplicationController
   Dropio::Config.api_key = "72851f00745cdfd0e87207219f3106bcc9927f66"
   def index
     drop = Dropio::Drop.find("fzf2n2l")
+     @drop = drop
     if false 
     @keys = []
     @howmany = drop.asset_count
-    @drop = drop
+   
     pages = (drop.asset_count / 30) + 1
     if !drop.blank?
       (pages).times { |i|
