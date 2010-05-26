7657de80e78a4a2d23f270d69d3193be0752e83b
Eric Skiff <eric@dropio.com>
2010-05-26 17:13:03 -0400

    Add info about configuring this app for your own use

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -4,25 +4,27 @@ class DropsController < ApplicationController
   require 'dropio'
   
   def initialize
-    ######################
-    #Production
-    Dropio::Config.api_key = "348df82fc24bd1eff27084a7b5f876afe61d9d7b"
-    Dropio::Config.version = "3.0"
-    @manager_api_token = "dq7yf4m3qx"
-    @output_locations = ["DropioS3","sl_0bb68ad0233c012db8e7fffb204ba9a3"]
-    @enabled_cdns = ["akamai", "voxel", "limelight"]
-  
-    ######################
-    #Staging
-    #Dropio::Config.base_url = "http://stage-drop.io"
-    #Dropio::Config.api_url = "http://stage-api.drop.io"
-    #Dropio::Config.upload_url = "http://stage-assets.drop.io/upload"
-    #Dropio::Config.api_key = "d4bf89d218cb982ddffdeef8a0144b57f88896c6"
-    #Dropio::Config.version = "3.0"
-    #@manager_api_token = "uxd1dxumgi"
-    #@output_locations = ["sl_2e2f29b01a82012da16efe5f8802078d"]
-    #@enabled_cdns = ["akamai", "voxel", "limelight"]
     
+    ######################
+    #MAIN CONFIGURATION (required)
+    Dropio::Config.api_key = "YOUR_API_KEY"
+    @manager_api_token = "YOUR_MANAGER_API_TOKEN"
+    
+    ################################
+    #ADDITIONAL CONFIGURATION (optional)
+    
+    ######################
+    #API Version 3.0 is required for using our Roles->Locations structure  
+    Dropio::Config.version = "3.0"
+    
+    ######################
+    #An array of available storage locations. You can create your own storage
+    #locations in the  http://manager.drop.io API tab
+    @output_locations = ["DropioS3"]
+    
+    ######################
+    #Which CDN links to display
+    @enabled_cdns = ["akamai", "voxel", "limelight"]
     
   end
 
