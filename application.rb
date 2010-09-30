d5c43a955e3b799cf35b0bd217ce0a76e75633ac
Bryan Woods <bryan@dropio.com>
2010-09-30 18:24:15 -0400

    Improving show_large_thumbnail helper

--- application.rb
+++ application.rb
@@ -25,14 +25,17 @@ end
 
 helpers do
   def show_large_thumbnail(asset)
-    large_thumbnail = lambda do
-      asset.roles.select{ |role| role["name"] == "large_thumbnail"}
-    end.call.first['locations']
-    begin
-      if asset.type == "image" and large_thumbnail.first["status"] == "complete"
-        "<img src='#{large_thumbnail.first['file_url']}' />"
+    if asset.type == "image"
+      large_thumbnail = lambda do
+        asset.roles.select{ |role| role["name"] == "large_thumbnail" }
+      end.call.first['locations']
+      begin
+        if large_thumbnail.first["status"] == "complete"
+          "<img src='#{large_thumbnail.first['file_url']}' />"
+        end
+      rescue Exception => e
+        puts "Caught exception: #{e}"
       end
-    rescue Exception
     end
   end
 end
